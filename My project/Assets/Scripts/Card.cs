4d056f6ef9e9494626bca0787c1759a634563129
fraav <franco.valenzuelan@estudiantes.arcos.cl>
2025-07-03 22:10:43 -0400

    jijiji
    algo de iluminación, maqueta y filtros.

--- My project/Assets/Scripts/Card.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewCard", menuName = "Cards/Card")]
-public class Card : ScriptableObject
-{
-    public string cardID;         // Por ejemplo: "A", "B", etc.
-    public Sprite artwork;        // Imagen de la carta
-    public string description;    // Opcional
-}
