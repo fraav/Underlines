5935697a4e26c0fc64f6276b9e227c6558802497
TheBigShaq115 <164810288+TheBigShaq115@users.noreply.github.com>
2025-07-14 22:00:26 -0400

    AHHH FRANCOOOOOOOO

--- Assets/Scripts_Card/HealthSystem.cs
+++ Assets/Scripts_Card/HealthSystem.cs
@@ -24,8 +24,7 @@ public class HealthSystem : MonoBehaviour
     void Start()
     {
         CurrentHealth = MaxHealth;
-
-        OnHealthChanged.Invoke(CurrentHealth);
+        OnHealthChanged.Invoke(CurrentHealth);  // esta linea
     }
 
     public void SetMaxHealth(int maxHealth)
