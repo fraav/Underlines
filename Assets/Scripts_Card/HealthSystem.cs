fedfb4f1fc3bcf9ea159bfd44245130547a9c0fa
fraav <franco.valenzuelan@estudiantes.arcos.cl>
2025-07-14 23:59:13 -0400

    lucesListas

--- Assets/Scripts_Card/HealthSystem.cs
+++ Assets/Scripts_Card/HealthSystem.cs
@@ -24,8 +24,6 @@ public class HealthSystem : MonoBehaviour
     void Start()
     {
         CurrentHealth = MaxHealth;
-
-        OnHealthChanged.Invoke(CurrentHealth);
     }
 
     public void SetMaxHealth(int maxHealth)
